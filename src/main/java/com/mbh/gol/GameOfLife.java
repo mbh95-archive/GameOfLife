c83c22e94602419af18fb02cdc8b4c217dcb46ed
mbh95 <matthewbh95@gmail.com>
2018-02-13 23:55:53 -0800

    Implemented step logic. Also added a helper method to the Cell class to get the set of neighboring cells of a cell.

--- src/main/java/com/mbh/gol/GameOfLife.java
+++ src/main/java/com/mbh/gol/GameOfLife.java
@@ -4,6 +4,7 @@ import com.google.auto.value.AutoValue;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GameOfLife implements IGameOfLife {
 
@@ -22,7 +23,31 @@ public class GameOfLife implements IGameOfLife {
     }
 
     public void step() {
+        Set<Cell> nextLiveCells = new HashSet<>();
 
+        // Get the set of live cells which will stay alive for the next generation.
+        Set<Cell> stillAliveCells = liveCells.stream()
+                .filter(liveCell -> {
+                    long liveNeighborCount = liveCell.getNeighbors().stream()
+                            .filter(neighborCell -> liveCells.contains(neighborCell))
+                            .count();
+                    return liveNeighborCount == 2 || liveNeighborCount == 3;
+                }).collect(Collectors.toSet());
+        nextLiveCells.addAll(stillAliveCells);
+
+        // Get the set of dead cells which will come alive for the next generation.
+        Set<Cell> risingDeadCells = liveCells.stream()
+                .flatMap(liveCell -> liveCell.getNeighbors().stream())
+                .filter(neighborCell -> !liveCells.contains(neighborCell))
+                .filter(deadCell -> {
+                    long liveNeighborCount = deadCell.getNeighbors().stream()
+                            .filter(neighborCell -> liveCells.contains(neighborCell))
+                            .count();
+                    return liveNeighborCount == 3;
+                }).collect(Collectors.toSet());
+        nextLiveCells.addAll(risingDeadCells);
+
+        liveCells = nextLiveCells;
     }
 
     @AutoValue
@@ -30,7 +55,22 @@ public class GameOfLife implements IGameOfLife {
         static Cell create(int r, int c) {
             return new AutoValue_GameOfLife_Cell(r, c);
         }
-       abstract int r();
-       abstract int c();
+
+        abstract int r();
+
+        abstract int c();
+
+        private Set<Cell> getNeighbors() {
+            Set<Cell> neighborSet = new HashSet<>();
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++) {
+                    if (dr == 0 && dc == 0) {
+                        continue;
+                    }
+                    neighborSet.add(Cell.create(r() + dr, c() + dc));
+                }
+            }
+            return neighborSet;
+        }
     }
 }
